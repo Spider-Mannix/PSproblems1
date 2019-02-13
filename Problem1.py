aeb190c06030275c80c7fa3f30a66da975ad0b36
Dave <dave90mannix@gmail.com>
2019-02-13 23:27:45 +0000

    improved print sections

--- Problem1.py
+++ Problem1.py
@@ -1,5 +1,5 @@
 #ask for number, create variables
-x = int(input("Please input a postive integer:"))
+x = int(input("Please input a postive integer: "))
 ans = 0
 i = 1
 
@@ -8,6 +8,6 @@ while i <= x:
   ans = ans + i
   i = i + 1
 
-print(ans)
+print("The sum of all integers between 1 and {} (inclusive) is: ".format(x), ans)
 
 #if type(x)! = int or >=0 "not positive integer"
