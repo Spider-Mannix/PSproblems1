f9d006891394d131d210eb96f075984c00c3245f
Dave <dave90mannix@gmail.com>
2019-03-09 20:49:01 +0000

    works on Saturday

--- Problem2.py
+++ Problem2.py
@@ -1,2 +1,16 @@
 #checking commit
 #use datetime
+import datetime
+
+#set variable
+day = datetime.datetime.today().weekday()
+
+#check and print
+if  day == 1:
+  print("Today is Tuesday, which starts with a 'T'.")
+
+elif day == 3:
+  print("Today is Thursday, which starts with a 'T'.")
+
+else:
+  print("Today does not start with 'T'.")
