7ff010a0ba956e516cb08938c8ba7202ca3fe050
Dave <dave90mannix@gmail.com>
2019-03-09 19:34:39 +0000

    adding P2

--- Problem1.py
+++ Problem1.py
@@ -1,13 +1,27 @@
-#ask for number, create variables
-x = int(input("Please input a postive integer: "))
+# ask for number 
+
+while True:
+    try:
+        x = int(input("Please input a postive integer: "))
+        #check if positive 
+        if x <= 0:
+            print("We're looking for a whole number greater than 0")
+            x = int(input("Please input a postive integer: "))
+            if x > 0:
+                break   
+            break
+    except ValueError:
+        print("We're looking for a whole number greater than 0")
+
+# "\n"
+
+#create variables
 ans = 0
 i = 1
 
-#from lecture video:
+# from lecture video:
 while i <= x:
-  ans = ans + i
-  i = i + 1
+    ans = ans + i
+    i = i + 1
 
 print("The sum of all integers between 1 and {} (inclusive) is: ".format(x), ans)
-
-#if type(x)! = int or >=0 "not positive integer"
