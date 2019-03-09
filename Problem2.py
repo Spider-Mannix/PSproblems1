286addec936b62452f6effb2544faafbe51da9bf
Dave <dave90mannix@gmail.com>
2019-03-09 21:26:40 +0000

    now using strftime()

--- Problem2.py
+++ Problem2.py
@@ -1,16 +1,14 @@
-#checking commit
+#program to check first letter of the day of the week
+
 #use datetime
 import datetime
 
-#set variable
-day = datetime.datetime.today().weekday()
+#set variable for today
+day = datetime.datetime.today().strftime('%A')
 
 #check and print
-if  day == 1:
-  print("Today is Tuesday, which starts with a 'T'.")
-
-elif day == 3:
-  print("Today is Thursday, which starts with a 'T'.")
+if  day[0] == "T":
+  print("Today is", day, ", which starts with a 'T'.")
 
 else:
-  print("Today does not start with 'T'.")
+  print("Today is", day, ", which does not start with 'T'.")
