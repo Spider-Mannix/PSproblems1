6a829d75dbc240ec2ede181f1fc2a46fcf75999f
Dave <dave90mannix@gmail.com>
2019-03-09 21:31:16 +0000

    comma string

--- Problem2.py
+++ Problem2.py
@@ -6,9 +6,13 @@ import datetime
 #set variable for today
 day = datetime.datetime.today().strftime('%A')
 
+#comma as string to tidy whitespace
+comma =","
+
 #check and print
 if  day[0] == "T":
-  print("Today is", day, ", which starts with a 'T'.")
+  print("Today is", day + comma, " which starts with a 'T'.")
 
 else:
-  print("Today is", day, ", which does not start with 'T'.")
+  print("Today is", day + comma, " which does not start with 'T'.")
+
