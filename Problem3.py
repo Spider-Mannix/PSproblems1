9216344b96019b71b1a05b0b8da270f154df5fab
Dave <dave90mannix@gmail.com>
2019-03-15 22:46:59 +0000

    define range

--- Problem3.py
+++ Problem3.py
@@ -1,7 +1,12 @@
 #print numbers between 1000 and 10000 divisible by 6 but not 12
 
-numbers = range(1000,1000)
-print(numbers[3])
+#numbers = list(range(1000,1000))
+#print(numbers)
 #x = len(numbers)
 #print(x)
 #print(numbers[x]-1)
+
+nums = range(1000,10000)
+
+for n in nums:
+    print(n)
