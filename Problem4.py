b381afe485355fe79daacfa1d5af270dfbf7d0de
Dave <dave90mannix@gmail.com>
2019-03-15 23:55:22 +0000

    first attempt successful

---
+++ Problem4.py
@@ -0,0 +1,12 @@
+#successive calculation down to value of 1
+
+x = int(input("Please input a postive integer: "))
+
+while x > 1:
+    if x % 2:
+        x = (x*3) + 1
+    else: 
+        #not(x % 2):
+        x = x/2
+    print(x)
+print(x)
