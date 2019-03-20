d6a61ec44f63ed3dfed9bbcd5526b455d11baab7
Dave <dave90mannix@gmail.com>
2019-03-20 23:11:06 +0000

    must get every second entry

--- Problem6.py
+++ Problem6.py
@@ -1,3 +1,10 @@
 #output every second word of an input string
 
 #use whitespace as a separator? Then while loop?
+
+x = input("Please enter a sentence")
+
+end = x.split()
+
+#for i in end, i+2
+    print(end[i])
