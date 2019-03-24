5305ae7f6657e236fb54f4d76825902a7177c313
Dave <dave90mannix@gmail.com>
2019-03-24 15:02:51 +0000

    need to iterate up to x

--- Problem5.py
+++ Problem5.py
@@ -3,3 +3,7 @@
 #a prime number is divisible only by itself and 1 - use modulus?
 
 x = int(input("Please input a postive integer: "))
+
+#iterate between 1 and x and check modulus
+i = 1
+for i < x, i +=1:
