33c9d441dc069e10e452b093a011702bb9e46b74
Dave <dave90mannix@gmail.com>
2019-03-24 17:08:12 +0000

    added check for 0 and 1

--- Problem5.py
+++ Problem5.py
@@ -4,6 +4,9 @@
 
 x = int(input("Please input a postive integer: "))
 
+if x == 1 or x==0:
+    x = int(input("A prime number, by definition, must be greater than 1. Please enter a whole number greater than 1:"))
+    
 #iterate between 1 and x and check modulus
 i = 1
 while i <= x:
