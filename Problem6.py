ed62f8049ea8a2ae9f572643804555b139f399b4
Dave <dave90mannix@gmail.com>
2019-03-24 17:35:47 +0000

    working

--- Problem6.py
+++ Problem6.py
@@ -4,8 +4,11 @@
 
 x = input("Please enter a sentence:")
 
+#split function separates string at whitespace
 y = x.split()
+#take every second component in list y
 z = y[::2]
 
 for i in z:
-    print(i)
+    print(i, end=" ")
+#end command stops printing on new line
