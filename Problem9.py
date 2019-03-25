77918361333aef32478e9932ec584f51da057b00
Dave <dave90mannix@gmail.com>
2019-03-25 23:10:42 +0000

    open text file with input

--- Problem9.py
+++ Problem9.py
@@ -1,3 +1,7 @@
 #output every second line from a text file
 
 #read text file then split like problem 6 but with new line operator e.g. split(/n)
+txt = str(input("File name to open?"))
+lines = open(txt,'r')
+
+print(lines.read())
