3eaf83562c440957b21769b07cc1999c8ac74180
Dave <dave90mannix@gmail.com>
2019-03-27 20:23:08 +0000

    adapted from lecture

--- Problem7.py
+++ Problem7.py
@@ -1,1 +1,11 @@
-#approximation of the square root of a loating point number
+#approximation of the square root of a floating point number
+
+#Newtonian root from lecture video:
+
+a = float(input("Approximate the square root of [a positive number]: "))
+est = float(input("What do you think the answer is? "))
+
+while abs((est**2)-a) > 0.01:
+    est -= ((est**2)-a)/(2*est)
+
+print(f"The square root of {a} is approximately {est}, within +/-0.01.")
