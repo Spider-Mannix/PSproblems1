5ef604d3b244071a03167867e7f5e1fdd5a3e7f6
Dave <dave90mannix@gmail.com>
2019-03-28 23:33:08 +0000

    added comments

--- Problem10.py
+++ Problem10.py
@@ -1,18 +1,21 @@
 #plot function of x, x^2 and 2^x in range [0,4]
 
 #use pyplot
-
 import matplotlib.pyplot as pl
 
+#generate required range, which is also function y = x
 x = range(0,5)
+
+#function y = x^2
 sq = [s*s for s in x]
+
+#function y = 2x
 twox = [2**a for a in x]
 
+#plot each function using pyplot
 pl.plot(x)
-#pl.show()
-
 pl.plot(sq)
-#pl.show()
-
 pl.plot(twox)
+
+#show is used to display graph
 pl.show()
