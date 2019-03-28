a2d154de87936c08a79b3c82211a271352772b3d
Dave <dave90mannix@gmail.com>
2019-03-28 23:12:37 +0000

    added comments

--- Problem3.py
+++ Problem3.py
@@ -3,10 +3,12 @@
 #create range
 nums = range(1000,10000)
 
-#use modulo
+#use modulus operator
 for n in nums:
+    #check division of 6
     if n % 6: 
         continue
+    #check division of 12    
     elif not(n % 12):
         continue
     else:
