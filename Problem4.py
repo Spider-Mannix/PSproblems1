f34eeb59c1a715a5628dc189010688d86ae2f5b0
Dave <dave90mannix@gmail.com>
2019-03-28 23:25:37 +0000

    added comments

--- Problem4.py
+++ Problem4.py
@@ -1,14 +1,17 @@
-#successive calculation down to value of 1
+#successive calculation down to value of 1 using input number
 
+#request number
 x = int(input("Please input a postive integer: "))
 
+#insert conditions
 while x > 1:
+
+    #check if odd
     if x % 2:
         x = (x*3) + 1
+
+    #if even   
     else: 
-        #not(x % 2):
         x = x/2
     print(x)
 print(x)
-
-#put in failsafes
