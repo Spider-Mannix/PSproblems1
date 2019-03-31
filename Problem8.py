f80ab6ee3d488ec12f4399a7c0ba2a7b0dfd49cb
Dave <dave90mannix@gmail.com>
2019-03-31 20:12:44 +0100

    comments

--- Problem8.py
+++ Problem8.py
@@ -1,8 +1,10 @@
-#today's date in format: Monday, January 10th 2019 at 1:15pm”.
+#today's date in format: Monday, January 10th 2019 at 1:15pm.
 
 #use datetime
 import datetime
 
+#take today's date
 day = datetime.datetime.today()
 
+#format as required using strftime
 print(day.strftime("%A, %B %dth %Y at %I:%M%p"))
